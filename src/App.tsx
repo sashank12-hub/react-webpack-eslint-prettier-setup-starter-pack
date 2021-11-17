b34c82d16aaee69db86b169740dd36280b3102be
Chavaly Sai Sashank <chasaisa@publicisgroupe.net>
2021-11-17 16:14:43 +0530

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import "./styles.css";
 function App() {
-    return (
-        <div>
-          <h1>React</h1>  
-        </div>
-    )
+  return (
+    <div>
+      <h1>React {process.env.name}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
